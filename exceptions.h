20eae2031aaae18da22c5ec504f43e6b64c24454
oersted <ezeiza.j@gmail.com>
2015-09-26 20:16:07 +0200

    Defined a simple exception system to make the code more clean.

---
+++ exceptions.h
@@ -0,0 +1,65 @@
+/*
+ * exceptions.h
+ *
+ *  Created on: 26 Sep 2015
+ *      Author: oersted
+ */
+
+/*
+ * ERR_T, ERR_N and OK can be defined by the user of this header before
+ * including it.
+ *
+ * In fact, it is encouraged that they are defined explicitely in the src file.
+ */
+
+/*
+ * The type of the global variable that will store the error values.
+ */
+#ifndef ERR_T
+#define ERR_T int
+#endif
+
+/*
+ * The name reserved to store the error values, this name shouln't be used in
+ * the src file that includes this header.
+ */
+#ifndef ERR_N
+#define ERR_N ERR_4578919
+#endif
+
+// Declaration of the error value container
+ERR_T ERR_N;
+
+/*
+ * If a function returns this value, it will mean that it has been executed
+ * without any issue.
+ */
+#ifndef OK
+#define OK 0
+#endif
+
+/*
+ * If the given expression returns a non OK value, the program will jump to the
+ * EXCEPT block with the given error_value.
+ */
+#define TRY(error_value, expression) \
+	if (expression == OK) { \
+		ERR_N = error_value; \
+		goto except; \
+	} \
+
+
+/*
+ * The given case_block defines what actions to perform for each possible error
+ * value.
+ *
+ * If the program reaches the except block, the respective error value will be
+ * returned. If not, OK will be returned.
+ */
+#define EXCEPT(case_block) \
+	return OK; \
+	except: \
+		switch(ERR_N) { \
+		case_block \
+		} \
+		return ERR_N;
