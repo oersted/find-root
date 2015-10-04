269986c591e7f1b1451d6ed1deea573fc8e6e05c
oersted <ezeiza.j@gmail.com>
2015-10-04 10:16:15 +0200

    Made all error codes positive instead of netagive.
    Just personal preference.

--- src/findroot.c
+++ src/findroot.c
@@ -23,14 +23,14 @@ ERR_T norm(int dim, double* x, double* n) {
 		norma(dim, x, n);
 	} else {
 		gsl_vector_view x_gsl = gsl_vector_view_array(x, dim);
-		TRY(-1, (*n = gsl_blas_dnrm2(&x_gsl.vector)) >= 0)
+		TRY(1, (*n = gsl_blas_dnrm2(&x_gsl.vector)) >= 0)
 	}
 
 	return OK;
 
 	EXCEPT: {
 		switch(ERR_V) {
-			case -1:
+			case 1:
 				fprintf(stderr, "[x] Ezin izan da norma kalkulatu.\n");
 				break;
 		}
@@ -55,8 +55,8 @@ ERR_T findroot(int dim, double tol, double* x0, double* x) {
 	fx = NULL;
 	jx = NULL;
 
-	TRY(-1, (fx = (double*) malloc(dim * sizeof(double))) != NULL)
-	TRY(-2, (jx = (double*) malloc(dim * dim * sizeof(double))) != NULL)
+	TRY(1, (fx = (double*) malloc(dim * sizeof(double))) != NULL)
+	TRY(2, (jx = (double*) malloc(dim * dim * sizeof(double))) != NULL)
 
 	gsl_set_error_handler_off();
 
@@ -82,7 +82,7 @@ ERR_T findroot(int dim, double tol, double* x0, double* x) {
 	// x0 == a
 
 	x = x0;
-	TRY(-3, norm(dim, x, &errorea) == OK)
+	TRY(3, norm(dim, x, &errorea) == OK)
 
 	// x == a
 	// x0 == a
@@ -91,8 +91,8 @@ ERR_T findroot(int dim, double tol, double* x0, double* x) {
 		f(dim, x, fx);
 		jakobiarra(dim, x, jx);
 
-		TRY(-4, gsl_linalg_LU_decomp(&jx_gsl.matrix, p, &s) == OK)
-		TRY(-5,
+		TRY(4, gsl_linalg_LU_decomp(&jx_gsl.matrix, p, &s) == OK)
+		TRY(5,
 			gsl_linalg_LU_solve(
 				&jx_gsl.matrix, p, &fx_gsl.vector, &x0_gsl.vector) == OK
 		)
@@ -100,9 +100,9 @@ ERR_T findroot(int dim, double tol, double* x0, double* x) {
 		// x == a
 		// x0 == c
 
-		TRY(-6, norm(dim, x0, &errorea) == OK)
+		TRY(6, norm(dim, x0, &errorea) == OK)
 
-		TRY(-7, gsl_vector_sub(&x_gsl.vector, &x0_gsl.vector) == OK)
+		TRY(7, gsl_vector_sub(&x_gsl.vector, &x0_gsl.vector) == OK)
 
 		// x == b
 		// x0 == c
@@ -112,32 +112,32 @@ ERR_T findroot(int dim, double tol, double* x0, double* x) {
 
 	EXCEPT: {
 		switch(ERR_V) {
-			case -1:
-			case -2:
+			case 1:
+			case 2:
 				fprintf(stderr,
 						"[x] Ezin izan da memoria nahikoa erreserbatu.\n");
 				break;
-			case -3:
+			case 3:
 				fprintf(stderr,
 						"[x] Hasierako puntuaren norma kalkulatzean errore "
 						"kritiko bat egon da.\n");
 				break;
-			case -4:
+			case 4:
 				fprintf(stderr,
 						"[x] Ezin izan da JX * x = FX ekuazio sistema "
 						"linealaren LU deskonposaketa egin.\n");
 				break;
-			case -5:
+			case 5:
 				fprintf(stderr,
 						"[x] Ezin izan da JX * x = FX ekuazio sistema lineala "
 						"ebatzi LU deskonposaketa erabiliz\n");
 				break;
-			case -6:
+			case 6:
 				fprintf(stderr,
 						"[x] JX * x = FX ekuazio sistema linealaren emaitzaren "
 						"norma kalkulatzean errore kritiko bat egon da.");
 				break;
-			case -7:
+			case 7:
 				fprintf(stderr,
 						"[x] Bektoreen arteko kenketa egitean errore kritiko "
 						"bat egon da");
@@ -163,15 +163,15 @@ int main(int argc, char** argv) {
 	x0 = NULL;
 
 	// Save the conf file path
-	TRY(-1, argc == 2)
+	TRY(1, argc == 2)
 	path = argv[1];
 
 	// Get conf file data
-	TRY(-2, datuak_lortu(path, &dim, &x0, &tol) > 0)
+	TRY(2, datuak_lortu(path, &dim, &x0, &tol) > 0)
 
 	// Find root
-	TRY(-3, (x = (double*) malloc(dim * sizeof(double))) != NULL)
-	TRY(-4, findroot(dim, tol, x0, x) == OK)
+	TRY(3, (x = (double*) malloc(dim * sizeof(double))) != NULL)
+	TRY(4, findroot(dim, tol, x0, x) == OK)
 
 	// Output result
 	printf("Emaitza: (");
@@ -184,19 +184,19 @@ int main(int argc, char** argv) {
 
 	EXCEPT: {
 		switch(ERR_V) {
-			case -1:
+			case 1:
 				printf("Usage: findroot path\n");
 				break;
-			case -2:
+			case 2:
 				fprintf(stderr,
 						"[x] Ezin izan da konfigurazio fitxategia ondo "
 						"irakurri.\n");
 				break;
-			case -3:
+			case 3:
 				fprintf(stderr,
 						"[x] Ezin izan da memoria nahikoa erreserbatu.\n");
 				break;
-			case -4:
+			case 4:
 				fprintf(stderr, "[x] Ezin izan da emaitza kalkulatu.\n");
 				break;
 		}
